b97f856638c44215c32c7a44c0e5ff9d1ba5a99c
mcculbr <mcculbr@8ec5e4ca-6654-e54a-8ea6-c57665c7f23a>
2011-06-23 21:31:58 +0000

    Alpha beta pruning fully implemented.
    git-svn-id: http://gwnp-server/repos/Programming/Work/C%23%20Gaming/XNA%20Projects/Flowers@303 8ec5e4ca-6654-e54a-8ea6-c57665c7f23a

--- Flowers/Flowers/Difficulties.cs
+++ Flowers/Flowers/Difficulties.cs
@@ -82,68 +82,11 @@ namespace Flowers {
 
 	#region Impossible difficulty
 	public class ImpossibleDifficulty : Difficulty {
-		#region Class variables
-		private const int INFINITY = 10000;
-		private const int MASTER_ALPHA = -2;
-		private const int MASTER_BETA = 2;
-		#endregion Class variables
-
 		#region Support methods
-		private int max(int a, int b) {
-			return (a > b ? a : b);//if a is more than b, return a, else return b
-		}
-
-		private int min(int a, int b) {
-			return (a < b ? a : b);// if a is less than b, return a, else return b
-		}
-
-		private int alphaBeta(Flower.FlowerType[] board, StateManager.TurnType turn, int alpha, int beta) {
-			Winner winner;
-			if (LogicUtils.isGameOver(board, out winner)) {
-				if (winner.winningType == LogicUtils.COMPUTERS_TYPE) {
-					return 1;
-				} else if (winner.winningType == LogicUtils.PLAYERS_TYPE) {
-					return -1;
-				} else {
-					return 0;
-				}
-			}
-			int score;
-			Flower.FlowerType[] cloned;
-			for (int i = 0; i < board.Length; i++) {
-				if (board[i] == Flower.FlowerType.None) {// get valid moves
-					cloned = LogicUtils.cloneFlowerTypes(board);
-					if (StateManager.TurnType.Computers == turn) {
-						cloned[i] = LogicUtils.COMPUTERS_TYPE;
-						score = alphaBeta(cloned, StateManager.TurnType.Players, alpha, beta);
-						if (score > alpha) {
-							alpha = score;
-						}
-						if (alpha >= beta) {
-							return alpha;
-						}
-					} else {
-						cloned[i] = LogicUtils.PLAYERS_TYPE;
-						score = alphaBeta(cloned, StateManager.TurnType.Computers, alpha, beta);
-						if (score < beta) {
-							beta = score;
-						}
-						if (alpha >= beta) {
-							return beta;
-						}
-					}
-				}
-			}
-			if (turn == StateManager.TurnType.Computers) {
-				return alpha;
-			} else {
-				return beta;
-			}
-		}
 		//Alpha: a score the computer knows with certanty it can achieve
 		//Beta: a score the human knows with certanty it can achieve
 		//If beta becomes <= alpha, further investigation is useless
-		private int miniMax(Flower.FlowerType[] types, Flower.FlowerType turn) {
+		private int miniMax(Flower.FlowerType[] types, Flower.FlowerType turn, int alpha, int beta) {
 			int bestMove;
 			int move;
 			Winner winner;
@@ -157,9 +100,11 @@ namespace Flowers {
 				}
 			} else {
 				if (LogicUtils.COMPUTERS_TYPE.Equals(turn)) {
-					bestMove = -INFINITY;
+					//bestMove = -INFINITY;
+					bestMove = alpha;
 				} else {
-					bestMove = INFINITY;
+					//bestMove = INFINITY;
+					bestMove = beta;
 				}
 				Flower.FlowerType[] cloned = null;
 				Flower.FlowerType inversedTurn = EnumUtils.inverseValue<Flower.FlowerType>(turn);
@@ -168,20 +113,41 @@ namespace Flowers {
 						cloned = LogicUtils.cloneFlowerTypes(types);
 						// get our result
 						cloned[i] = turn;
-						move = miniMax(cloned, inversedTurn);
+						move = miniMax(cloned, inversedTurn, alpha, beta);
 						// interpret our result
-						if (LogicUtils.COMPUTERS_TYPE.Equals(turn) && move > bestMove) {
+						if (LogicUtils.COMPUTERS_TYPE == turn) {
+							if (move > alpha) {
+								alpha = move;
+							}
+							if (alpha >= beta) {
+								return alpha;
+							}
+						} else {
+							if (move < beta) {
+								beta = move;
+							}
+							if (alpha >= beta) {
+								return beta;
+							}
+						}
+						/*if (LogicUtils.COMPUTERS_TYPE.Equals(turn) && move > bestMove) {
 							bestMove = move;
 						} else if (LogicUtils.PLAYERS_TYPE.Equals(turn) && move < bestMove) {
 							bestMove = move;
-						}
+						}*/
 					}
 				}
 			}
-			return bestMove;
+			if (turn == LogicUtils.COMPUTERS_TYPE) {
+				return alpha;
+			} else {
+				return beta;
+			}
+			//return bestMove;
 		}
 
 		public override int getMove(Flower[] board) {
+			const int INFINITY = 10000;
 			int result = -INFINITY;
 			Flower.FlowerType[] types = LogicUtils.getFlowerTypes(board);
 			// figure out if this is our first move
@@ -197,6 +163,8 @@ namespace Flowers {
 			} else {
 				int move;
 				int bestMove = -INFINITY;
+				int alpha = -INFINITY;
+				int beta = INFINITY;
 				Flower.FlowerType[] cloned;
 				Winner winner;
 				for (int i = 0; i < types.Length; i++) {
@@ -209,7 +177,7 @@ namespace Flowers {
 								break;// we found a win so no need to continue looping
 							}
 						}
-						move = miniMax(cloned, LogicUtils.PLAYERS_TYPE);//run the algorithm
+						move = miniMax(cloned, LogicUtils.PLAYERS_TYPE, alpha, beta);//run the algorithm
 						if (move > bestMove) {// interpret the results
 							result = i;
 							bestMove = move;
